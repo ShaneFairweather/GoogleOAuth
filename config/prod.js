a54df5ebb4eb1be1731985d5ee080aa8f67d59f0
Smafair <shanefairweather@gmail.com>
2017-11-16 09:46:10 -0800

    facebook oauth

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,8 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
+    facebookClientID: process.env.FACEBOOK_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 }
